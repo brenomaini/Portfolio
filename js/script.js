55646a42d17688e14d556825832445ee1d0e9d49
Breno Maini <dev.brenomaini@gmail.com>
2022-12-26 23:01:18 -0300

    load animations

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@
   element.addEventListener('click',moveIfClicked);
  });
 
+
+ const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+ });
 // 
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
+
  
 
 function moveIfClicked(clickedItem){
