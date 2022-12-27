7e244d149d16470412cdf0ac6f429a32936aa78c
Breno Maini <dev.brenomaini@gmail.com>
2022-12-26 21:40:36 -0300

    mudei tudo quase

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
  document.querySelectorAll('.item').forEach(function listenToClick(element) {
-  element.addEventListener('click',move);
+  element.addEventListener('click',moveIfClicked);
  });
 
-function move(clickedItem){
+// 
+ 
+
+function moveIfClicked(clickedItem){
 const checkIfClickedItemIsAlreadyAtCenter = clickedItem.target.classList[1] == 'positionCenter'
 
   if(checkIfClickedItemIsAlreadyAtCenter){
@@ -24,3 +27,20 @@ clickedKnowledgeBar.classList.add('fill');
 clickedKnowledgeBar.style.animationName=('fillIn');
 
 }
+
+/*DESISTI*/
+
+// let item = document.querySelectorAll('input').forEach(function checkIfChecked(e){
+  //   e.addEventListener('change',moveIfLogoClicked)
+  // })
+
+// function moveIfLogoClicked(e){
+// const logoClicked = e.target.id.toString() + "KB";
+// let momentItemAtCenter = document.querySelector('.positionCenter');
+// let clickedLogo = document.getElementById(`${logoClicked}`);
+// let clickedPosition = clickedLogo.classList[1];
+
+// momentItemAtCenter.classList.replace('positionCenter',`${clickedPosition}`);
+// clickedLogo.classList.replace(`${clickedPosition}`,'positionCenter');
+
+// }
