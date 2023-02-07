7a6ffddcb71a751ac7db15c2ce5676bb785dcb15
Breno Maini <dev.brenomaini@gmail.com>
2023-02-07 14:30:48 -0300

    Alterações no corpo e projetos

--- js/script.js
+++ js/script.js
@@ -1,51 +1,47 @@
- document.querySelectorAll('.item').forEach(function listenToClick(element) {
-  element.addEventListener('click',moveIfClicked);
- });
+document.querySelectorAll(".item").forEach(function listenToClick(element) {
+  element.addEventListener("click", moveIfClicked);
+});
 
-
- const observer = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
-    if(entry.isIntersecting){
-      entry.target.classList.add('show');
-    }else{
-      entry.target.classList.remove('show');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
- });
-// 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=>observer.observe(el));
+});
+//
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
- 
+function moveIfClicked(clickedItem) {
+  const checkIfClickedItemIsAlreadyAtCenter =
+    clickedItem.target.classList[1] == "positionCenter";
 
-function moveIfClicked(clickedItem){
-const checkIfClickedItemIsAlreadyAtCenter = clickedItem.target.classList[1] == 'positionCenter'
-
-  if(checkIfClickedItemIsAlreadyAtCenter){
+  if (checkIfClickedItemIsAlreadyAtCenter) {
     return;
   }
 
-let clickedKnowledgeBar = clickedItem.target.childNodes[3].childNodes[1].firstElementChild
-let PositionOfClickedItem = clickedItem.target.classList[1];
+  let PositionOfClickedItem = clickedItem.target.classList[1];
 
-let momentItemAtCenter = document.querySelector('.positionCenter');
-let knowledgeBarOfMomentItemAtCenter = momentItemAtCenter.childNodes[3].childNodes[1].firstElementChild;
+  let momentItemAtCenter = document.querySelector(".positionCenter");
 
-knowledgeBarOfMomentItemAtCenter.style.animationName=('fillOut');   
-
-momentItemAtCenter.classList.replace('positionCenter', `${PositionOfClickedItem}`);
-clickedItem.target.classList.replace(`${clickedItem.target.classList[1]}`,'positionCenter');
-
-clickedKnowledgeBar.classList.add('fill');
-clickedKnowledgeBar.style.animationName=('fillIn');
-
+  momentItemAtCenter.classList.replace(
+    "positionCenter",
+    `${PositionOfClickedItem}`
+  );
+  clickedItem.target.classList.replace(
+    `${clickedItem.target.classList[1]}`,
+    "positionCenter"
+  );
 }
 
 /*DESISTI*/
 
 // let item = document.querySelectorAll('input').forEach(function checkIfChecked(e){
-  //   e.addEventListener('change',moveIfLogoClicked)
-  // })
+//   e.addEventListener('change',moveIfLogoClicked)
+// })
 
 // function moveIfLogoClicked(e){
 // const logoClicked = e.target.id.toString() + "KB";
