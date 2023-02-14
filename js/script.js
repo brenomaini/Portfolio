b02a6189d3947434a7e025ffd1c824014aefc8ef
Breno Maini <dev.brenomaini@gmail.com>
2023-02-14 14:58:46 -0300

    Form submit function included

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const LanguageFlags = document.querySelectorAll(".languageFlag");
+let actualFlag = "ptbr";
 
 const observePageFocus = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -24,7 +25,8 @@ function changeLanguage(flagClicked) {
   selectedLanguage.contains("selected")
     ? null
     : document.querySelector(".selected").classList.remove("selected"),
-    selectedLanguage.add("selected");
+    selectedLanguage.add("selected"),
+    (actualFlag = selectedLanguage[1]);
 }
 
 const knowledgeItemClicked = document.querySelectorAll(".item");
@@ -110,5 +112,54 @@ function mouseOverProject(selectedProject) {
       "projectHidden",
       "projectShow"
     );
+  }
+}
+
+const form = document.getElementById("ContactInfo");
+
+form.addEventListener("submit", formSubmit);
+
+async function formSubmit(e) {
+  const name = document.getElementById("Name").value;
+  const email = document.getElementById("Email").value;
+  const info = document.getElementById("Info").value;
+  e.preventDefault();
+
+  try {
+    await fetch(
+      "https://docs.google.com/forms/u/1/d/e/1FAIpQLScffuQoQycipC5f9ABkk6dUDgKhVab6SJ1JJnTXqhclSYZUbg/formResponse",
+      {
+        credentials: "include",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Alt-Used": "docs.google.com",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1",
+        },
+        referrer:
+          "https://docs.google.com/forms/d/e/1FAIpQLScffuQoQycipC5f9ABkk6dUDgKhVab6SJ1JJnTXqhclSYZUbg/viewform?fbzx=3758937148958631696",
+        body: `entry.1850254109=${name}&entry.476346648=${email}.com&entry.1275965327=${info}`,
+        method: "POST",
+        mode: "no-cors",
+      }
+    ).then((res) => {
+      form.reset();
+
+      actualFlag == "ptbr"
+        ? alert("Obrigado, entrarei em contato o mais breve possível!")
+        : alert("Thank You, i'll contact you ASAP!");
+    });
+
+    window.location.href = "#";
+  } catch (error) {
+    alert(error + ":" + "Error please refresh your browser");
   }
 }
